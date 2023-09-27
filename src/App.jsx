8787b5439214d2c0467e1c02e2d3b14d3d668cbd
xhiep <xhhiep@gmail.com>
2023-09-27 21:25:26 +0700

    fix dark light mode

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,40 @@ import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
 import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
 import { pink } from '@mui/material/colors'
 import SvgIcon from '@mui/material/SvgIcon'
-import TabIndicator from '@mui/material/Tabs'
+import Typography from '@mui/material/Typography'
+import { useColorScheme } from '@mui/material/styles'
 
 function App() {
+
+  const { mode, setMode } = useColorScheme()
+
+
   function HomeIcon(props) {
     return (
       <SvgIcon {...props}>
         <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
       </SvgIcon>
-    );
+    )
+  }
+  const ModToggle = () => {
+    return (
+      <Button
+        onClick={() => {
+          setMode(mode === 'light' ? 'dark' : 'light')
+        }}
+      >
+        {mode === 'light' ? 'Turn dark' : 'Turn light'}
+      </Button>
+    )
   }
   return (
     <>
+      <Typography variant="body2" color='text.secondary'>test </Typography>
+      <Button variant="contained" color="success">Button</Button>
       <AccessAlarmIcon />
+      <br />
+      <ModToggle />
+      <br />
       <ThreeDRotation />
       <HomeIcon />
       <HomeIcon color="primary" />
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import CssBaseline from '@mui/material/CssBaseline'
 import App from './App.jsx'
+import theme from './theme.js'
+import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CssBaseline />
-    <App />
+    <CssVarsProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </CssVarsProvider>
   </React.StrictMode>
 )
