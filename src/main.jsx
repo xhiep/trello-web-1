4efd7945ea0b367ea7fc223ee0075d01cef1f19c
xhiep <xhhiep@gmail.com>
2023-10-02 20:27:23 +0700

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import CssBaseline from '@mui/material/CssBaseline'
 import App from './App.jsx'
-import theme from './theme.js';
+import theme from './theme.js'
 import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
