1970c55e41e475a2a4277a1775cc062aa731a356
xhiep <xhiep@pop-os.localdomain>
2023-10-17 10:35:07 +0700

    add layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
-import { Button, FormControl, InputLabel, MenuItem, Select } from '@mui/material'
-import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
-import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
-import { pink } from '@mui/material/colors'
-import SvgIcon from '@mui/material/SvgIcon'
-import Typography from '@mui/material/Typography'
+import { Box, Container, FormControl, InputLabel, MenuItem, Select } from '@mui/material'
 import { useColorScheme } from '@mui/material/styles'
 
 function App() {
 
   const { mode, setMode } = useColorScheme()
 
-  function SelectSmall() {
+  function ModeSelect() {
     const handleChange = (event) => {
       setMode(event.target.value)
     }
@@ -31,46 +26,31 @@ function App() {
       </FormControl>
     )
   }
-
-  function HomeIcon(props) {
-    return (
-      <SvgIcon {...props}>
-        <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
-      </SvgIcon>
-    )
-  }
-  const ModToggle = () => {
-    return (
-      <Button
-        onClick={() => {
-          setMode(mode === 'light' ? 'dark' : 'light')
-        }}
-      >
-        {mode === 'light' ? 'Turn dark' : 'Turn light'}
-      </Button>
-    )
-  }
   return (
-    <>
-      <SelectSmall />
-      <Typography variant="body2" color='text.secondary'>test </Typography>
-      <Button variant="contained" color="success">Button</Button>
-      <AccessAlarmIcon />
-      <br />
-      <ModToggle />
-      <br />
-      <ThreeDRotation />
-      <HomeIcon />
-      <HomeIcon color="primary" />
-      <HomeIcon color="secondary" />
-      <HomeIcon color="success" />
-      <HomeIcon color="action" />
-      <HomeIcon color="disabled" />
-      <HomeIcon sx={{ color: pink[500] }} />
-      <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
-    </>
+    <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
+      <Box sx={{
+        backgroundColor: 'primary.light',
+        width: '100%',
+        height: (theme) => theme.trello.appBarHeight,
+        display: 'flex',
+        alignItems: 'center'
+      }}>
+        <ModeSelect />
+      </Box>
+      <Box sx={{
+        backgroundColor: 'primary.dark',
+        width: '100%',
+        height: theme => theme.trello.boardBarHeight,
+        display: 'flex',
+        alignItems: 'center'
+      }}>board bar</Box>
+      <Box sx={{
+        backgroundColor: 'primary.main',
+        height: theme => `calc(100vh - ${theme.trello.boardBarHeight} - ${theme.trello.appBarHeight})`,
+        display: 'flex',
+        alignItems: 'center'
+      }}>board bar content</Box>
+    </Container >
   )
 }
 
